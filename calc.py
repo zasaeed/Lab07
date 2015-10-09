c4e68119efc6f6821fa6eec8075653ec8f5de287
Zaid <zsaeed@purdue.edu>
2015-10-09 14:46:04 -0400

    Added Module Function

--- calc.py
+++ calc.py
@@ -33,6 +33,12 @@ def mult(a, b):
 def div(a, b):
     return a / b
 
+# Module function
+# a -- dividend
+# b -- divisor
+def mod(a, b):
+	return a % b
+
 
 # -------------------------------------------------------- #
 
@@ -67,6 +73,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
+		elif (op == "%"):
+			print "Modulus: ", mod(a, b)
         else:
             print "Invalid operation..."
 
